5e75ee0294be3eb361d8f604f2fc704e1212bde3
Cristian Jeldes <cristian.jeldes@usach.cl>
2017-04-08 16:26:58 -0300

    Added base for templates and for static files

--- fbproject/fbproject/settings.py
+++ fbproject/fbproject/settings.py
@@ -123,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__), '../../static/').replace('\\','/')) 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
--- fbproject/project/views.py
+++ fbproject/project/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth import authenticate
+from django.utils import timezone
+import time
+import requests
+import os
+from project.models import *
+import math
+from collections import namedtuple
+global basePath
+class IndexView(View):
+	def get(self, request):
+		return render(request, 'project/index.html',{})
